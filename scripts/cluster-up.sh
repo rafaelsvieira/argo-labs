2d22cac04c1ee0fa8310bfeab1f06f15f72fef95
Rafael <rafael.souzavieira@gmail.com>
2026-05-17 12:32:59 -0300

    Start cloud-provider-kind during cluster bootstrap

--- scripts/cluster-up.sh
+++ scripts/cluster-up.sh
@@ -23,3 +23,11 @@ kubectl --context "kind-${CLUSTER_NAME}" cluster-info
 echo
 echo "Nodes:"
 kubectl --context "kind-${CLUSTER_NAME}" get nodes -o wide
+
+VERSION="$(basename $(curl -s -L -o /dev/null -w '%{url_effective}' https://github.com/kubernetes-sigs/cloud-provider-kind/releases/latest))"
+
+echo "Starting cloud-controller-manager in a container for cloud-provider-kind version ${VERSION}..."
+
+docker run -d --name cloud-provider-kind --rm --network host -v /var/run/docker.sock:/var/run/docker.sock registry.k8s.io/cloud-provider-kind/cloud-controller-manager:${VERSION}
+
+echo "Cluster '${CLUSTER_NAME}' is up and running!"
